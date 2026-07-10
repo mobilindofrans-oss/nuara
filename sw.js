a30503a39e2033c7e16e0105a0867be8bef64be0
Ajat Rosadi <mobilindofrans@gmail.com>
2026-07-10 11:01:21 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,8 @@ const FILES = [
   './style.css',
   './app.js',
   './icon.svg',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
 ];
 
 self.addEventListener('install', e => {
